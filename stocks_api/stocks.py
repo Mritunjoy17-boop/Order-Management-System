1af082c7fa6419a6d00a917034ec2f23f0a7cdff
sayakcodez <sayak@codez.in>
2025-09-13 17:40:18 +0530

    multiple changes in stocks.py

--- stocks_api/stocks.py
+++ stocks_api/stocks.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI,HTTPException,Depends,status,Header
-from fastapi.exception_handlers import RequestValidationError
+from fastapi.exceptions import RequestValidationError
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
@@ -15,9 +15,9 @@ app = FastAPI()
 
 #pydantic models
 class StocksRequest(BaseModel):
-    product_code : str
-    moulder_id : str
-    current_godown : str
+    product_code : str | None = None
+    moulder_id : str | None = None
+    current_godown : str | None = None
 
 class StocksResponse(BaseModel):
     message : dict
@@ -33,7 +33,7 @@ def get_tokens(authorization : str = Header(...)):
     return token
 
 @app.post("/", response_model=StocksResponse)
-async def user_products(data: StocksRequest, db=Depends(connect_db), token : str = Depends(get_tokens)):
+async def user_stocks(data: StocksRequest, db=Depends(connect_db), token : str = Depends(get_tokens)):
     db_cursor = db.cursor(dictionary=True)
     db_cursor.execute(
         "SELECT jwt_status FROM user_jwt WHERE jwt_token =%s",
@@ -44,37 +44,36 @@ async def user_products(data: StocksRequest, db=Depends(connect_db), token : str
         product_code = data.product_code
         moulder_id = data.moulder_id
         current_godown = data.current_godown
-        print(product_code,moulder_id,current_godown)
 
-        # db_cursor.execute(
-        #     "SELECT unit_name,unit_friendly_name,is_active FROM units WHERE unit_name =%s",
-        #     (unit_name,)
-        # )
-        # units_query_data = db_cursor.fetchall()
-        # if units_query_data:
-        #     units_list = []
-        #     for units_dict in units_query_data:
-        #         units_list.append(units_dict)
+        db_cursor.execute(
+            "SELECT barcode_id,lot_code,series_number,product_code,moulder_id,date_of_print,date_of_manufacture,date_of_sell,current_godown,date_time_last_update FROM stock WHERE product_code =%s AND moulder_id =%s AND current_godown =%s",
+            (product_code,moulder_id,current_godown)
+        )
+        stocks_query_data = db_cursor.fetchall()
+        if stocks_query_data:
+            stocks_list = []
+            for stocks_dict in stocks_query_data:
+                stocks_list.append(stocks_dict)
 
-        #     success_message = f"Units data found successfully"
-        #     db_cursor.close()
+            success_message = f"Stocks data found successfully"
+            db_cursor.close()
 
-        #     return_dict = {
-        #         "units_data" : units_list
-        #     }
+            return_dict = {
+                "stocks_data" : stocks_list
+            }
 
-        #     json_response = {
-        #         "msg": success_message,"status":"Success","data":return_dict
-        #     }
+            json_response = {
+                "msg": success_message,"status":"Success","data":return_dict
+            }
 
-        #     return {"message": json_response}
-        # else:
-        #     db_cursor.close()
-        #     failure_msg = "Invalid unit name"
-        #     failure_response = {
-        #         "msg": failure_msg,"status":"Failure","data":{}
-        #     }
-        #     return {"message": failure_response}
+            return {"message": json_response}
+        else:
+            db_cursor.close()
+            failure_msg = "Invalid product code or moulder id or current godown"
+            failure_response = {
+                "msg": failure_msg,"status":"Failure","data":{}
+            }
+            return {"message": failure_response}
     else:
         db_cursor.close()
         failure_msg = "Token not valid, login again"
