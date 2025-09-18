d363e6f8c6bc23365b0340836e19bc39b5286024
sayakcodez <sayak@codez.in>
2025-09-18 12:46:56 +0530

    inward outward api introduced

--- master_api.py
+++ master_api.py
@@ -7,8 +7,7 @@ from godown_api.godown import app as godown_app
 from moulder_api.moulder import app as moulder_app
 from units_api.units import app as units_app
 from stocks_api.stocks import app as stocks_app
-from stocks_api.stocks_movement import app as stocks_movement_app
-from stocks_api.stocks_reconcilation import app as stocks_reconcilation_app
+from stocks_api.submit_inward_ourward import app as submit_inward_ourward_app
 from logout_api.logout import app as logout_app
 
 app = FastAPI()
@@ -21,6 +20,5 @@ app.mount("/godown", godown_app)
 app.mount("/moulder", moulder_app)
 app.mount("/units", units_app)
 app.mount("/stocks", stocks_app)
-app.mount("/stocks_movement", stocks_movement_app)
-app.mount("/stocks_reconcilation", stocks_reconcilation_app)
+app.mount("/submit_inward_ourward", submit_inward_ourward_app)
 app.mount("/logout", logout_app)
