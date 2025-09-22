8a59b50ad05e9390e2fe7b5f918db4660fbed68b
sayakcodez <sayak@codez.in>
2025-09-22 16:07:32 +0530

    changes in variance_stocks_recon api

--- stocks_api/variance_stocks_recon.py
+++ stocks_api/variance_stocks_recon.py
@@ -45,71 +45,38 @@ async def variance_stocks_reconcilation(data: List[VarianceRequest], db=Depends(
     token_query = db_cursor.fetchone()
     if token_query and token_query['jwt_status'] == 'valid':
         actual_data_list = [actual_data.dict() for actual_data in data]
-        print(actual_data_list)
-
-        # godown_id = data.godown_id
-        # category_id = data.category_id
-
-        # #inward query
-        # db_cursor.execute(
-        #     "SELECT prod.product_name, prod.product_code, COUNT(sm.record_id) AS product_count FROM stock_movement sm INNER JOIN products prod ON sm.product_code = prod.product_code INNER JOIN category cat ON prod.product_category = cat.category_id WHERE sm.godown_id =%s AND sm.movement_type = 'inward' AND cat.category_id =%s GROUP BY prod.product_code, prod.product_name",
-        #     (godown_id,category_id)
-        # )
-        # inward_reconcilation_data = db_cursor.fetchall()
-
-        # #outward query
-        # db_cursor.execute(
-        #     "SELECT prod.product_name, prod.product_code, COUNT(sm.record_id) AS product_count FROM stock_movement sm INNER JOIN products prod ON sm.product_code = prod.product_code INNER JOIN category cat ON prod.product_category = cat.category_id WHERE sm.godown_id =%s AND sm.movement_type = 'outward' AND cat.category_id =%s GROUP BY prod.product_code, prod.product_name",
-        #     (godown_id,category_id)
-        # )
-        # outward_reconcilation_data = db_cursor.fetchall()
-
-        # expected_data_list = []
-        # if inward_reconcilation_data and outward_reconcilation_data:
-        #     product_inward_dict = {}
-        #     for inward_data in inward_reconcilation_data:
-        #         product_inward_dict[inward_data['product_code']] = inward_data
-
-        #     product_outward_dict = {}
-        #     for outward_data in outward_reconcilation_data:
-        #         product_outward_dict[outward_data['product_code']] = outward_data
-
-        #     all_keys = product_inward_dict.keys() | product_outward_dict.keys()
-
-        #     for key in all_keys:
-        #         if key in product_inward_dict and key in product_outward_dict:
-        #             product = product_inward_dict[key].copy()
-        #             product['product_count'] = product_inward_dict[key]['product_count'] - product_outward_dict[key]['product_count'] 
-        #             expected_data_list.append(product)
-        #         elif key in product_inward_dict:
-        #             expected_data_list.append(product_inward_dict[key].copy())
-        #         else:
-        #             expected_data_list.append(product_outward_dict[key].copy())
-        # elif inward_reconcilation_data:
-        #     expected_data_list = inward_reconcilation_data
-        # elif outward_reconcilation_data:
-        #     expected_data_list = outward_reconcilation_data
             
-        # if expected_data_list:
-        #     success_message = f"Expected data of stocks reconcilation found successfully"
-        #     db_cursor.close()
+        if actual_data_list:
+            variance_list = []
+            for temp_data in actual_data_list:
+                temp_list_data = {
+                    'product_name' : temp_data['product_name'],
+                    'product_code' : temp_data['product_code'],
+                    'product_variance' : temp_data['expected_product_count'] - temp_data['actual_product_count']
+                }
+                variance_list.append(temp_list_data)
 
-        #     return_dict = {
-        #         "expected_reconcilation_data" : expected_data_list
-        #     }
+            print(variance_list)
 
-        #     json_response = {
-        #         "msg": success_message,"status":"Success","data":return_dict
-        #     }
+            # success_message = f"Variance data of stocks reconcilation found successfully"
+            # db_cursor.close()
 
-        #     return {"message": json_response}
-        # else:
-        #     db_cursor.close()
-        #     failure_msg = "No expected data found for reconcilation"
-        #     failure_response = {
-        #         "msg": failure_msg,"status":"Failure","data":{}
-        #     }
-        #     return {"message": failure_response}
+            # return_dict = {
+            #     "variance_reconcilation_data" : variance_list
+            # }
+
+            # json_response = {
+            #     "msg": success_message,"status":"Success","data":return_dict
+            # }
+
+            # return {"message": json_response}
+        else:
+            db_cursor.close()
+            failure_msg = "No expected data found for reconcilation variance"
+            failure_response = {
+                "msg": failure_msg,"status":"Failure","data":{}
+            }
+            return {"message": failure_response}
     else:
         db_cursor.close()
         failure_msg = "Token not valid, login again"
