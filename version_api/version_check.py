d241dfb6ac5f76e10719416470571466bb69b990
sayakcodez <sayak@codez.in>
2025-09-23 13:46:07 +0530

    changes in version_check api

--- version_api/version_check.py
+++ version_api/version_check.py
@@ -22,62 +22,40 @@ class VersionRequest(BaseModel):
 class VersionResponse(BaseModel):
     message : dict
 
-#Dependency to extract tokens from headers
-def get_tokens(authorization : str = Header(...)):
-    if not authorization.startswith("Bearer "):
-        raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail = "Invalid authorization header format"
-        )
-    token = authorization.split(" ")[1]
-    return token
-
 @app.post("/", response_model=VersionResponse)
-async def user_products(data: VersionRequest, db=Depends(connect_db), token : str = Depends(get_tokens)):
+async def user_products(data: VersionRequest, db=Depends(connect_db)):
+    version = data.version
+    version_type = data.version_type
+    device_info = data.device_info
+
     db_cursor = db.cursor(dictionary=True)
+
     db_cursor.execute(
-        "SELECT jwt_status FROM user_jwt WHERE jwt_token =%s",
-        (token,)
+        "SELECT app_version,app_version_type,app_url FROM app_version_check;",
     )
-    token_query = db_cursor.fetchone()
-    if token_query and token_query['jwt_status'] == 'valid':
-        version = data.version
-        version_type = data.version_type
-        device_info = data.device_info
+    version_query_data = db_cursor.fetchone()
+    if version_query_data:
+        is_update_required = False
+        if float(version.strip()) > float(version_query_data['app_version'].strip()):
+            is_update_required = True
 
-        db_cursor.execute(
-            "SELECT app_version,app_version_type,app_url FROM app_version_check;",
-        )
-        version_query_data = db_cursor.fetchone()
-        if version_query_data:
-            is_update_required = False
-            if float(version.strip()) > float(version_query_data['app_version'].strip()):
-                is_update_required = True
+        success_message = f"Version data found successfully"
+        db_cursor.close()
 
-            success_message = f"Version data found successfully"
-            db_cursor.close()
+        return_dict = {
+            "is_update_required" : is_update_required,
+            "app_url" : version_query_data['app_url'],
+            "is_sync_required" : False
+        }
 
-            return_dict = {
-                "is_update_required" : is_update_required,
-                "app_url" : version_query_data['app_url'],
-                "is_sync_required" : False
-            }
+        json_response = {
+            "msg": success_message,"status":"Success","data":return_dict
+        }
 
-            json_response = {
-                "msg": success_message,"status":"Success","data":return_dict
-            }
-
-            return {"message": json_response}
-        else:
-            db_cursor.close()
-            failure_msg = "No record of version exists"
-            failure_response = {
-                "msg": failure_msg,"status":"Failure","data":{}
-            }
-            return {"message": failure_response}
+        return {"message": json_response}
     else:
         db_cursor.close()
-        failure_msg = "Token not valid, login again"
+        failure_msg = "No record of version exists"
         failure_response = {
             "msg": failure_msg,"status":"Failure","data":{}
         }
