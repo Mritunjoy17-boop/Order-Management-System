548d8feec5a3a2ced02e16a3afcd64e03ef6a25f
sayakcodez <sayak@codez.in>
2025-10-07 16:36:58 +0530

    changes in login api

--- login_api/login.py
+++ login_api/login.py
@@ -61,52 +61,49 @@ async def user_login(data: LoginRequest, request: Request, db=Depends(connect_db
         )
         token_query_result = db_cursor.fetchall()
         if token_query_result:
-            print(token_query_result)
-            print(device_id)
-
             device_id_flag = 0
             for list_item in token_query_result:
                 if list_item['device_id'] == device_id:
                     device_id_flag = 1
                     break
 
-            print(device_id_flag)
+            if device_id_flag:
+                print(123)
+                
+                db_cursor.execute(
+                    "UPDATE user_jwt SET jwt_token =%s, jwt_status = 'valid' WHERE mobile_number =%s and device_id = %s",
+                    (access_token, mobile_number, device_id)
+                )
+            else:
+                db_cursor.execute(
+                    "INSERT INTO user_jwt(mobile_number,jwt_token,jwt_status,device_id) VALUES(%s,%s,'valid',%s)",
+                    (mobile_number, access_token, device_id)
+                )    
+        else:
+            db_cursor.execute(
+                "INSERT INTO user_jwt(mobile_number,jwt_token,jwt_status,device_id) VALUES(%s,%s,'valid',%s)",
+                (mobile_number, access_token, device_id)
+            )
 
-            # if token_query_result.get('mobile_number','') and token_query_result.get('device_id',''):
-            #     db_cursor.execute(
-            #         "UPDATE user_jwt SET jwt_token =%s, jwt_status = 'valid' WHERE mobile_number =%s and device_id = %s",
-            #         (access_token, mobile_number, device_id)
-            #     )
-            # else:
-            #     db_cursor.execute(
-            #         "INSERT INTO user_jwt(mobile_number,jwt_token,jwt_status,device_id) VALUES(%s,%s,'valid',%s)",
-            #         (mobile_number, access_token, device_id)
-                # )    
-        # else:
-        #     db_cursor.execute(
-        #         "INSERT INTO user_jwt(mobile_number,jwt_token,jwt_status,device_id) VALUES(%s,%s,'valid',%s)",
-        #         (mobile_number, access_token, device_id)
-        #     )
+        db.commit()
+        db_cursor.close()
 
-        # db.commit()
-        # db_cursor.close()
+        return_dict = {
+            "user_data" : {
+                "mobile_number" : mobile_number,
+                "user_name" : user,
+                "user_type" : user_type,
+                "is_active" : is_active,
+            },
+            "access_token": "Bearer " + access_token,
+            "token_type":"bearer"
+        }
 
-        # return_dict = {
-        #     "user_data" : {
-        #         "mobile_number" : mobile_number,
-        #         "user_name" : user,
-        #         "user_type" : user_type,
-        #         "is_active" : is_active,
-        #     },
-        #     "access_token": "Bearer " + access_token,
-        #     "token_type":"bearer"
-        # }
+        json_response = {
+            "msg": memo_message,"status":"Success","data":return_dict
+        }
 
-        # json_response = {
-        #     "msg": memo_message,"status":"Success","data":return_dict
-        # }
-
-        # return {"message": json_response}
+        return {"message": json_response}
     else:
         db_cursor.close()
         failure_msg = "Invalid username or password"
