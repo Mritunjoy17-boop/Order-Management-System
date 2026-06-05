12ac0427b0aa6ee5898d50f3a0ce500b0ade65bd
sayakcodez <sayak@codez.in>
2026-06-05 11:30:13 +0000

    feat: Notification file changed

--- utils/notifications.py
+++ utils/notifications.py
@@ -1,38 +1,38 @@
 
 from firebase_admin import messaging
 
-def send_notification(token, title, body):
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title=title,
-            body=body
-        ),
-        token=token
-    )
+# def send_notification(token, title, body):
+#     message = messaging.Message(
+#         notification=messaging.Notification(
+#             title=title,
+#             body=body
+#         ),
+#         token=token
+#     )
 
-    response = messaging.send(message)
-    return response
+#     response = messaging.send(message)
+#     return response
 
 
-def send_bulk_notification(tokens, title, body):
+# def send_bulk_notification(tokens, title, body):
 
-    messages = [
-        messaging.Message(
-            notification=messaging.Notification(
-                title=title,
-                body=body
-            ),
-            token=token
-        )
-        for token in tokens
-    ]
+#     messages = [
+#         messaging.Message(
+#             notification=messaging.Notification(
+#                 title=title,
+#                 body=body
+#             ),
+#             token=token
+#         )
+#         for token in tokens
+#     ]
 
-    response = messaging.send_each(messages)
+#     response = messaging.send_each(messages)
 
-    return {
-        "success_count": response.success_count,
-        "failure_count": response.failure_count
-    }
+#     return {
+#         "success_count": response.success_count,
+#         "failure_count": response.failure_count
+#     }
 
 def send_order_notification(tokens, title, body, order_id):
 
